cdd4e6b1515872ac13441dfb5bb95f82fa0588dc
jacopo <jacopo.simonini@stud.unifi.it>
2024-10-10 10:03:17 +0200

    commit

---
+++ cmake-build-debug/CMakeFiles/ToDoList.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ToDoList.dir/main.cpp.obj"
+  "ToDoList.exe"
+  "ToDoList.exe.manifest"
+  "ToDoList.pdb"
+  "libToDoList.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ToDoList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
